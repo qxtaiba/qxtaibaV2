0d671adde129b16139c156caad5203cfefcb5664
Qutaiba Al-Nuaimy <qan205@nyu.edu>
2023-07-10 04:09:39 +0400

    flesh out about section

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -1,17 +1,34 @@
 import {
-    AboutWrapper,
-    HighlightAlt,
-    HighlightSpan,
+  AboutWrapper,
+  ProgressBar,
+  Link
   } from "../styles/About.styled";
   
   const About: React.FC = () => {
     return (
       <AboutWrapper data-testid="about">
-        <p>
-          hello <HighlightSpan>world</HighlightSpan>!
-        </p>
+      <div>
+        <pre>
+          &gt;   Name:         Qutaiba Al-Nuaimy
+          <br />
+          &gt;   Location:     London, United Kingdom &amp; Abu Dhabi, United Arab Emirates
+          <br />
+          <br />
+          &gt;   English:       []-<ProgressBar>#################################################</ProgressBar>-[]
+          <br />
+          &gt;   Arabic:        []-<ProgressBar>#################################################</ProgressBar>-[]
+          <br />
+          <br />
+          &gt;   Education:     Electrical and Software Engineering at <Link href="https://nyuad.nyu.edu">NYU(AD)</Link> 
+          <br />
+          &gt;   Employment:    Forward Deployed Software Engineer at <Link href="https://www.palantir.com">Palantir</Link> 
+          <br />
+          <br />
+        </pre>
+      </div>
       </AboutWrapper>
     );
   };
+  
   
   export default About;
--- src/components/styles/About.styled.tsx
+++ src/components/styles/About.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const AboutWrapper = styled.div`
-  margin-top: 0.25rem;
+  margin-top: 1.5rem;
   margin-bottom: 0.75rem;
   p {
     margin-top: 0.5rem;
@@ -15,4 +15,20 @@ export const HighlightSpan = styled.span`
 
 export const HighlightAlt = styled.span`
   font-weight: 700;
+`;
+
+export const ProgressBar = styled.span`
+  color: #00ff00;
+`;
+
+export const Link = styled.a`
+  color: #cbd5e1;
+  text-decoration: none;
+  line-height: 1.5rem;
+  white-space: nowrap;
+  border-bottom: 2px dashed #cbd5e1;
+
+  &:hover {
+    border-bottom-style: solid;
+  }
 `;
